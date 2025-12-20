4b985f154a8af3dc83cadb37bfc2a781f03abaa5
amenoni <andrewj.menoni@gmail.com>
2025-12-20 17:16:05 -0500

    add button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
-  const [count, setCount] = useState(0);
+  function beginCreate() {
+    alert("you clicked");
+  }
 
   return (
     <>
       <div className="app-container">
         <h1>Simple Savage</h1>
+        <Button text={"Build Character"} onClick={beginCreate} />
       </div>
     </>
   );
