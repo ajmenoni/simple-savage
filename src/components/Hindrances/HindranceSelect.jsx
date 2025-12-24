cc9d1e3c17eb48f798af291476473b86f608f44d
amenoni <andrewj.menoni@gmail.com>
2025-12-23 23:59:30 -0500

    update hindranceSelect

--- src/components/Hindrances/HindranceSelect.jsx
+++ src/components/Hindrances/HindranceSelect.jsx
@@ -3,20 +3,6 @@ import Button from "../Button/Button";
 import Card from "../Card";
 
 function HindranceSelect({ hindrances, toggleSelection, slideIn, onDone }) {
-  function handleToggle(e, hindrance) {
-    // Toggle global state
-    toggleSelection("hindrances", hindrance);
-
-    // Remove any lingering focus/active state on mobile by briefly focusing then blurring
-    try {
-      const el = e.currentTarget;
-      if (el && typeof el.focus === "function") el.focus();
-      if (el && typeof el.blur === "function") el.blur();
-    } catch (err) {
-      // ignore
-    }
-  }
-
   return (
     <div className={slideIn ? "slide-in-left" : "slide-out-left"}>
       <div className="items-container">
@@ -27,8 +13,7 @@ function HindranceSelect({ hindrances, toggleSelection, slideIn, onDone }) {
             <Card
               key={hindrance.id}
               className={`item-card ${isSelected ? "selected" : ""}`}
-              onClick={(e) => handleToggle(e, hindrance)}
-              tabIndex={-1}
+              onClick={() => toggleSelection("hindrances", hindrance)}
             >
               <div className="item-card-title">{hindrance.name}</div>
               <div>{hindrance.description}</div>
