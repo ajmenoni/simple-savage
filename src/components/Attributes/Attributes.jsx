05957c3bad5c118d2328ceeee5a3a17d8b164252
amenoni <andrewj.menoni@gmail.com>
2025-12-28 15:19:23 -0500

    update trait selects to be grid; add padding prop to Card

--- src/components/Attributes/Attributes.jsx
+++ src/components/Attributes/Attributes.jsx
@@ -6,28 +6,30 @@ import "../../styles/traitRow.css";
 
 function Attributes() {
   return (
-    <Card>
+    <Card padding="compact">
       <div className="slide slide-in-bottom">
         <h2>Attributes</h2>
         <div className="trait-rows">
           {attributes.map((attribute) => (
             <fieldset className="trait-row" key={attribute.id}>
-              <legend className="trait-name">{attribute.name}</legend>
-              {["d4", "d6", "d8", "d10", "d12"].map((die) => (
-                <React.Fragment key={`${attribute.id}-${die}`}>
-                  <input
-                    type="radio"
-                    id={`${attribute.id}-${die}`}
-                    name={`trait-${attribute.id}`}
-                    value={die}
-                  />
-                  <label
-                    htmlFor={`${attribute.id}-${die}`}
-                    className={`dice-toggle ${die}`}
-                    aria-label={`Select ${die} for ${attribute.name}`}
-                  ></label>
-                </React.Fragment>
-              ))}
+              <span className="trait-name">{attribute.name}</span>
+              <div className="dice-row">
+                {["d4", "d6", "d8", "d10", "d12"].map((die) => (
+                  <React.Fragment key={`${attribute.id}-${die}`}>
+                    <input
+                      type="radio"
+                      id={`${attribute.id}-${die}`}
+                      name={`trait-${attribute.id}`}
+                      value={die}
+                    />
+                    <label
+                      htmlFor={`${attribute.id}-${die}`}
+                      className={`dice-toggle ${die}`}
+                      aria-label={`Select ${die} for ${attribute.name}`}
+                    ></label>
+                  </React.Fragment>
+                ))}
+              </div>
             </fieldset>
           ))}
         </div>
